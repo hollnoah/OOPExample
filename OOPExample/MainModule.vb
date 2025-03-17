4fd27431eb4ea92b4aa04c189d54c68a53278a2b
Noah Holloway <noahholloway23@gmail.com>
2025-03-17 08:09:34 -0600

    Add project files.

---
+++ OOPExample/MainModule.vb
@@ -0,0 +1,11 @@
+﻿Module MainModule
+
+    Sub Main()
+        DoSomethingInMainModule()
+    End Sub
+
+    Sub DoSomethingInMainModule()
+        MsgBox("Hello from the MainModule!")
+    End Sub
+
+End Module
---
+++ OOPExample/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("OOPExample")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Razer")>
+<Assembly: AssemblyProduct("OOPExample")>
+<Assembly: AssemblyCopyright("Copyright © Razer 2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("bf680cbd-2515-45cd-9064-fc297fae41d4")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
